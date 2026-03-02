68d13e7127e24ef564ff57d5b9a6a5326d24d6e1
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2026-03-02 14:21:20 +0530

    changed databse postgre

--- server/crm_backend/settings.py
+++ server/crm_backend/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'crm_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
