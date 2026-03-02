ed4e51445f045c0de6cb2e33a535478051753199
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2026-03-02 13:47:00 +0530

    postgre initilization

--- client/crm-frontend/src/App.js
+++ client/crm-frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Register />} />
 
         <Route path="/login" element={<Login />} />
 
